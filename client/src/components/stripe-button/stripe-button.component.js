550b0169279b7810410038d6282640095cda97e8
Raymond Sotto <raymondjosephsotto@gmail.com>
2021-02-06 11:08:33 -0500

    remove gitignore files

--- client/src/components/stripe-button/stripe-button.component.js
+++ client/src/components/stripe-button/stripe-button.component.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51HpBtmBYN47iADi26QjZbnomg8bP0El8A4YOPQI4tBtofPqc0NJrv6HBzbkQ8eS4qPBDwj74r0OiGJ3YN7Jp2m4U00GdhvsJ6t'
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful')
+       axios({
+           url: 'payment',
+           method: 'post',
+           data: {
+               amount: priceForStripe,
+               token
+           }
+       })
+       .then(response => {
+           alert('Payment Successful')
+       })
+       .catch(error => {
+           console.log('Payment Error: ', JSON.parse(error));
+           alert(
+               'There was an issue with your payment. Please make sure you use the provided credit cart.'
+           )
+       })
     }
 
     return (
